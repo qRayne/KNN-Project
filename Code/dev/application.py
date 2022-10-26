d18eddd3e7310262191c1fedffa3ac389e8e892f
qRayne <kennafrayanerachid@gmail.com>
2022-10-26 08:11:31 -0400

    knn ui progress added

--- Code/dev/application.py
+++ Code/dev/application.py
@@ -15,14 +15,14 @@ class myApp(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
          
-        # Dataset
+        # ---------------------------- (Dataset) - Global menu--------------------------------#
         self.__group_data_set = QtWidgets.QGroupBox('Dataset')
         
         
         # Dataset - list deroulante
-        self.__menu_list_layout = QVBoxLayout()
-        self.__menu_list = QtWidgets.QComboBox()
-        self.__menu_list_layout.add_widget(self.__menu_list)
+        self.__menu_list_data_layout = QVBoxLayout()
+        self.__menu_data_list = QtWidgets.QComboBox()
+        self.__menu_list_data_layout.add_widget(self.__menu_data_list)
         
         
         # Dataset - included in dataset
@@ -60,37 +60,131 @@ class myApp(QtWidgets.QMainWindow):
         
         
         
-        # --------
-        central_layout = QVBoxLayout()
-        central_layout.add_layout(self.__menu_list_layout)
-        central_layout.add_widget(self.__included_in_dataset_box)
-        central_layout.add_widget(self.__transformation_box)
-        self.__group_data_set.set_layout(central_layout)
-      
-      
-      
+        central_layout_data = QVBoxLayout()
+        central_layout_data.add_layout(self.__menu_list_data_layout)
+        central_layout_data.add_widget(self.__included_in_dataset_box)
+        central_layout_data.add_widget(self.__transformation_box)
+        self.__group_data_set.set_layout(central_layout_data)
+ 
+
+        # ---------------------------- (single test) - Global menu --------------------------------#    
+        
+        self.__group_single_test = QtWidgets.QGroupBox('Single test')
+           
+        # single test - list deroulante
+        self.__menu_list_single_layout = QVBoxLayout()
+        self.__menu_single_list = QtWidgets.QComboBox()
+        self.__menu_list_single_layout.add_widget(self.__menu_single_list)
+
+        # single test - image
+        self.__image_single_layout = QVBoxLayout()
+        self.__single_background_color = QLabel()
+        self.__single_background_color.alignment = Qt.AlignCenter
+        self.__single_background_color.set_fixed_height(90)
+        
+
+        pixmap = QtGui.QPixmap(self.__single_background_color.size)
+        pixmap.fill(QtGui.QColor(56, 68, 110))
     
-        # single test 
-        self.__group_single_test = QtWidgets.QGroupBox('single test')
+        self.__single_background_color.set_pixmap(pixmap)
+
+        self.__image_single_layout.add_widget(self.__single_background_color)
+
+        # single test - Classify button
+        self.__classify_button_layout = QVBoxLayout()
+        self.__classify_button = QtWidgets.QPushButton('Classify')
+        self.__classify_button_layout.add_widget(self.__classify_button)
+
+        # champ text
+        self.__classify_info_layout = QVBoxLayout()
+
+        self.__classify_info = QLabel('star_5_050')
+        self.__classify_info.alignment = Qt.AlignCenter 
+        self.__classify_info_layout.add_widget(self.__classify_info)
+
+
+        central_layout_single = QVBoxLayout()
+        central_layout_single.add_layout(self.__menu_list_single_layout)
+        central_layout_single.add_layout(self.__image_single_layout)
+        central_layout_single.add_layout(self.__classify_button_layout)
+        central_layout_single.add_layout(self.__classify_info_layout)
+        self.__group_single_test.set_layout(central_layout_single)
         
         
+    
         
+        # ---------------------------- (KNN parametre) - Global menu --------------------------------#    
+
+        self.__group_knn_parameters = QtWidgets.QGroupBox('Knn parameters')
+        self.__knn_scrollbar = QtWidgets.QScrollBar()
+        self.__max_scrollbar = QtWidgets.QScrollBar() 
+        self.__parametre_info_layout = QVBoxLayout()
+
+    
+        scroll_1_layout = self.__create_channel('k =', self.__knn_scrollbar,75)
+        scroll_2_layout = self.__create_channel('kd =', self.__max_scrollbar,75)
+
+        self.__parametre_info_layout.add_layout(scroll_1_layout)
+        self.__parametre_info_layout.add_layout(scroll_2_layout)
+
+
+        central_layout_parametres = QVBoxLayout()
+        central_layout_parametres.add_layout(self.__parametre_info_layout)
+        self.__group_knn_parameters.set_layout(central_layout_parametres)
+     
+    
+        # ------------------------------------------ About ---------------------------------------#    
+        self.__about_button_layout = QVBoxLayout()
+        self.__about_button = QtWidgets.QPushButton('About')
         
+
+        # ------------------------------------------ GAME ---------------------------------------#    
+
+        self.__knn_view = QLabel("Knn")
+        self.__knn_view.set_fixed_width(600)
+        self.__knn_view.set_fixed_height(600)
+        self.__knn_view.alignment = Qt.AlignCenter
         
+
+        # ------------------------------------------ GLOBAL ---------------------------------------#    
+
+        self.__setting_box = QVBoxLayout()
+        self.__setting_box.add_widget(self.__group_data_set)
+        self.__setting_box.add_widget(self.__group_single_test)
+        self.__setting_box.add_widget(self.__group_knn_parameters)
+        self.__setting_box.add_widget(self.__about_button)
+  
+        self.__Knn_box = QVBoxLayout()
+        self.__Knn_box.add_widget(self.__knn_view)
         
-        
-        self.__group_knn_parameters = QtWidgets.QGroupBox('knn parameters')
-        
-        self.__principal_box = QVBoxLayout()
-        self.__principal_box.add_widget(self.__group_data_set)
-        self.__principal_box.add_widget(self.__group_single_test)
-        self.__principal_box.add_widget(self.__group_knn_parameters)
-        self.__principal_box.add_stretch()
-        
+        self.__principal_box = QHBoxLayout()
+        self.__principal_box.add_layout(self.__setting_box)
+        self.__principal_box.add_layout(self.__Knn_box)
+
+        self.set_window_title('Knn Image Classifaction')
+
         centrale_widget = QWidget()
         centrale_widget.set_layout(self.__principal_box)
         self.set_central_widget(centrale_widget)
       
+    def __create_channel(self, text, scroll, width):
+        title = QLabel(text)
+        value = QLabel('0')
+        
+        title.set_fixed_width(22)
+        scroll.minimum_width = 2 * width
+        scroll.orientation = Qt.Horizontal
+        scroll.set_range(0, 255)
+        scroll.value = 0
+        value.alignment = Qt.AlignCenter
+        value.set_fixed_width(22)
+           
+        layout = QHBoxLayout()
+        layout.add_widget(title)
+        layout.add_widget(value)
+        layout.add_widget(scroll)
+       
+        return layout
         
     
 def main():
