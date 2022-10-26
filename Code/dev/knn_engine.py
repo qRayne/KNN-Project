8344ad27a9df4e00d8c5a8f88286a45a33cce979
qRayne <kennafrayanerachid@gmail.com>
2022-10-26 12:25:01 -0400

    finished coding for today

--- Code/dev/knn_engine.py
+++ Code/dev/knn_engine.py
@@ -4,50 +4,99 @@ import random
 from re import X
 import numpy as np
 import klustr_utils
-
 class KNN:
-    def __init__(self,nDim):
-        self.__nDim = nDim
-        self.__image = None
+    def __init__(self):
+        self.__arrayImages = np.array([])
+        self.__x = np.array([])
+        self.__y = np.array([])
+        self.__z= np.array([])
+        self.__arrayDescripteur = np.array([]) # self.__x,self.__y,self.__z [x,y,z] pour chaque image
+        
+        
+    def init_matrice(self):
+        matrice = [[0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0],
+                   [0,0,1,1,1,1,1,1,0,0],
+                   [0,0,1,1,1,1,1,1,0,0],
+                   [0,0,1,1,1,1,1,1,0,0],
+                   [0,0,1,1,1,1,1,1,0,0],
+                   [0,0,1,1,1,1,1,1,0,0],
+                   [0,0,1,1,1,1,1,1,0,0],
+                   [0,0,0,0,0,0,0,0,0,0],
+                   [0,0,0,0,0,0,0,0,0,0]]
+        matriceNumpy = np.array(matrice)
+        return matriceNumpy.flatten()
+                
+                
+
         
     # 1 : Conversion png to ndarray
     def conversion_png_ndarray(self,dataImage):
-        qImage = klustr_utils.qimage_argb32_from_png_decoding(dataImage)
-        arrayBinary = klustr_utils.ndarray_from_qimage_argb32(qImage)
-        self.__image = arrayBinary
+        #qImage = klustr_utils.qimage_argb32_from_png_decoding(dataImage)
+        #arrayBinary = klustr_utils.ndarray_from_qimage_argb32(qImage)
+        #test 
+        arrayBinary = dataImage
+        self.__arrayImages = np.append(self.__arrayImages,arrayBinary)
         
-    # 2 : Utiliser les formules 
-    def aireForme(self):
-        return np.sum(np.count_nonzero(self.__image))
-    
-    def perimetreForme(self):
-        return np.sum(self.__image[:,1:] != self.__image[:,:-1]) + np.sum(self.__image[1:,:] != self.__image[:-1,:])
-    
-    def centroideForme(self):
-        c, r = np.meshgrid(np.arange(self.__image.shape[1]), np.arange(self.__image.shape[0]))
-        return (np.sum(r * self.__image), np.sum(c * self.__image)) / self.aireForme(self.__image)
+               
+    # 2 : Utiliser les formules
 
-    # ContourForme pour 1 image
-    def trouver_contour_forme_image(self):
-        return self.aireForme() / self.perimetreForme() ** 2
+    def aireForme(self,imageBinary):
+        return np.sum(np.count_nonzero(imageBinary))
     
+    def perimetreForme(self,imageBinary):
+        imageBinary2d = np.reshape(imageBinary, (-1, 2))
+        return np.sum(imageBinary2d[:,1:] != imageBinary2d[:,:-1]) + np.sum(imageBinary2d[1:,:] != imageBinary2d[:-1,:])
     
-    def trouverRatioForme(self):
-        centroide = self.centroideForme()
-        zeroPlusProche = np.abs(centroide - 0).argmin()
-        
-        
-        
-
-    
-    def trouverSommeDistances(self):
+    def centroideForme(self,imageBinary):
+        # imageBinary2d = np.reshape(imageBinary, (-1, 2))
+        # c, r = np.meshgrid(np.arange(imageBinary2d.shape[1]), np.arange(imageBinary2d.shape[0]))
+        # return (np.sum(r * imageBinary2d), np.sum(c * imageBinary2d)) / self.aireForme(imageBinary2d)
         pass
 
+    
+    def trouverContourFormeImage(self):
+        for imageBinary in self.__arrayImages:
+            print(imageBinary)
+            #self.__x  = np.append(self.__x,self.aireForme(imageBinary) / self.perimetreForme(imageBinary) ** 2)
+            
+    # def trouverRatioImage(self):
+    #     #for imageBinary in self.__arrayImages:
+    #     pass
+            
+              
+        
+    
+    # def centroideForme(self):
+    #     c, r = np.meshgrid(np.arange(self.__image.shape[1]), np.arange(self.__image.shape[0]))
+    #     return (np.sum(r * self.__image), np.sum(c * self.__image)) / self.aireForme(self.__image)
+
+    # # ContourForme pour 1 image
+    # def trouverContourFormeImage(self):
+    #     for imageBinary in self.__arrayImages:
+    #         np.append(self.__x,np.sum(np.count_nonzero(imageBinary)))
+            
+    
+    #     return self.aireForme() / self.perimetreForme() ** 2
+    
+    
+    # def trouverRatioForme(self):
+    #     centroide = self.centroideForme()
+    #     zeroPlusProche = np.abs(centroide).argmin()
+    #     zeroPlusLoin = np.abs()
+        
+        
+        
+
+    
+    # def trouverSommeDistances(self):
+    #     pass
 
 
-    # Reshape le data en dimension Ndim
-    def reshape_data(self):
-        pass
+
+    # # Reshape le data en dimension Ndim
+    # def reshapeData(self):
+    #     pass
         
         
 
@@ -106,7 +155,21 @@ class KNN:
     
         
 def main():
-    knn_engine = KNN(3)
+    knn_engine = KNN()
+    matrice = knn_engine.init_matrice()
+    matrice2 = knn_engine.init_matrice()
+
+    knn_engine.conversion_png_ndarray(matrice)
+    knn_engine.conversion_png_ndarray(matrice2)
+    
+    
+    print(matrice)
+    #knn_engine.aireForme
+    
+    print('Aire: ' + str(knn_engine.aireForme(matrice)))
+    #print('Centroide: ' + str(knn_engine.centroideForme(matrice)))
+    #print('Perimetre: ' + str(knn_engine.perimetreForme(matrice)))
+    knn_engine.trouverContourFormeImage()
     
 
 if __name__ == '__main__':
